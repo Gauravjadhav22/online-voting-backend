e249b04df87aecd0150b234b479a709a60b08ec6
gaurav jadhav <75011596+Gauravjadhav22@users.noreply.github.com>
2023-01-29 10:59:00 +0530

    Update AllowedOrigins.js

--- Config/AllowedOrigins.js
+++ Config/AllowedOrigins.js
@@ -1,7 +1,7 @@
 const AllowedOrigins = [
-    'https://online-voting-frontend-k32j-iamgaurav9130-gmailcom.vercel.app/',
-    'https://online-voting-frontend-k32j-8gvob2y8v-iamgaurav9130-gmailcom.vercel.app/',
-    'https://online-voting-frontend-k32j.vercel.app/',
+    'https://online-voting-frontend-k32j-iamgaurav9130-gmailcom.vercel.app',
+    'https://online-voting-frontend-k32j-8gvob2y8v-iamgaurav9130-gmailcom.vercel.app',
+    'https://online-voting-frontend-k32j.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
