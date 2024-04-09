e0c1faac320ae4d55e49cd33b12849932c227573
gaurav jadhav <gauravjadhav7512@gmail.com>
2024-04-09 20:04:57 +0530

    fixes

--- server.js
+++ server.js
@@ -1,54 +1,51 @@
-require("dotenv").config()
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const app = express()
-const Connect = require("./Db/Connect")
-const VoterRoute = require("./Routes/VotersRoute")
-const VoterRoomRoute = require("./Routes/VotingRoomRoute")
-const ErrorHandler = require("./Middleware/ErrorHandler")
-const NotFound = require("./Middleware/NotFound")
-const Credentials = require("./Middleware/Credentials")
-const CorsOptions = require("./Config/CorsOptions")
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const Connect = require("./Db/Connect");
+const VoterRoute = require("./Routes/VotersRoute");
+const VoterRoomRoute = require("./Routes/VotingRoomRoute");
+const ErrorHandler = require("./Middleware/ErrorHandler");
+const NotFound = require("./Middleware/NotFound");
+const Credentials = require("./Middleware/Credentials");
+const CorsOptions = require("./Config/CorsOptions");
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(Credentials)
+app.use(Credentials);
 
 //cross origin resource sharing
-app.use(cors(CorsOptions))
+app.use(cors(CorsOptions));
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 //middleware for json
-app.use(express.json())
+app.use(express.json());
 
 // middleware for cookies
-app.use(cookieParser())
-const port = process.env.PORT || 8080;
+app.use(cookieParser());
+const port = process.env.PORT || 8000;
 
-app.use('/welcome', (req, res) => {
-    console.log("fsdfs");
-    res.json({ "msg": "welcome to web app" })
-})
+app.use("/welcome", (req, res) => {
+  res.json({ msg: "welcome to web app" });
+});
 
-app.use('/api/voters', VoterRoute)
-app.use('/api/voterroom', VoterRoomRoute)
+app.use("/api/voters", VoterRoute);
+app.use("/api/voterroom", VoterRoomRoute);
 
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
-app.use(NotFound)
+app.use(NotFound);
 const Start = async () => {
-
-    try {
-
-        app.listen(port, console.log(`server is listening on ${port}..`))
-        await Connect(process.env.MONGO_URI).then(() => console.log("connected to db~..."))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
+  try {
+    app.listen(port, console.log(`server is listening on ${port}..`));
+    await Connect(process.env.MONGO_URI).then(() =>
+      console.log("connected to db~...")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 Start();
